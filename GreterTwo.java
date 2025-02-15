362ec1f4407120d02b49d0475194a0f2c6d5b625
Harshad <Harshad4697@gmail.com>
2025-02-15 11:21:15 +0530

    Add my fifigreter two  StarPattern2 progarm

---
+++ GreterTwo.java
@@ -0,0 +1,17 @@
+class GreterTwo
+{
+   public static void main(String args[])
+   {
+     int i=10;
+	 int b=20;
+	 if(i>b)
+	 {
+       System.out.println("i is gretr "+i);
+	 }
+	 else
+	 {
+	   System.out.println("B is gretr "+b);
+	 }
+   }
+}
+
